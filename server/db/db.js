18dbbfe8046acb4ea0b9b8de84a2d642aaf90313
Jeffrey Wood <jwfirenze@gmail.com>
2022-08-24 09:53:25 -0400

    db name is namespace-db

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const dbUrl = process.env.DB_URL || "postgres://localhost:5432/privatestore";
+const dbUrl = process.env.DB_URL || "postgres://localhost:5432/namespace-db";
 
 const db = new Sequelize(dbUrl, {
   logging: false,
