7ab20a774aecf42139b88169d5955fe57df0dc06
Jeffrey Wood <jwfirenze@gmail.com>
2022-08-26 13:07:55 -0400

    db port mapping to 5433

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const dbUrl =
   process.env.DATABASE_URL ||
   process.env.DOCKER_DATABASE_URL ||
-  "postgres://localhost:5432/namespace-db";
+  "postgres://localhost:5433/namespace-db";
 
 const db = new Sequelize(dbUrl, {
   logging: false,
