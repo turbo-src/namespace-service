44a010eae3c428d357e2c29043410ff845b455f4
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2023-07-20 13:17:57 -0400

    refactor: additional edge cases handled

--- test-server/findOrCreateRepo.js
+++ test-server/findOrCreateRepo.js
@@ -5,27 +5,39 @@ describe("findOrCreateRepo", function () {
 	it("should find or create a new repo in the database", async function () {
 		const createRes = await findOrCreateRepo(
 			/*repoName:*/ "tsrctester1/demo",
-			/*repoId:*/ "none"
+			/*repoId:*/ ""
 		);
 
 		const findRes = await findOrCreateRepo(
 			/*repoName:*/ "tsrctester1/demo",
-			/*repoId:*/ "none"
+			/*repoId:*/ ""
 		);
 
 		const findByIDRes = await findOrCreateRepo(
-			/*repoName:*/ "none",
+			/*repoName:*/ "",
 			/*repoId:*/ findRes.repoID
 		);
 
-		const error = await findOrCreateRepo(
+		const error403 = await findOrCreateRepo(
 			/*repoName:*/ "",
 			/*repoId:*/ ""
 		);
 
+		const error404 = await findOrCreateRepo(
+			/*repoName:*/ "tsrctester1/demo",
+			/*repoId:*/ "incorrectid1234"
+		);
+
+		// const error4042 = await findOrCreateRepo(
+		// 	/*repoName:*/ "incorrectrepo/name",
+		// 	/*repoId:*/ findRes.repoID
+		// );
+
 		assert.equal(createRes.status, 201, "Failed to create a repo");
 		assert.equal(findRes.status, 200, "Failed to find a repo by name");
 		assert.equal(findByIDRes.status, 200, "Failed to find a repo by id");
-		assert.equal(error.status, 403, "Failed to trigger an error based on bad arguments");
+		assert.equal(error403.status, 403, "Failed to trigger an error based on bad arguments");
+		assert.equal(error404.status, 404, "Failed to trigger an error based on incorrect combination of repoID and repoName");
+		// assert.equal(error4042.status, 404, "Failed to trigger an error based on incorrect combination of repoID and repoName");
 	});
 });
