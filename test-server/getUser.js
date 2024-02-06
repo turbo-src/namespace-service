98c75fdd31038162d605aa032b3f09e72f6ec796
7db9a <>
2024-02-06 12:01:57 -0500

    tests: assert get user.

--- test-server/getUser.js
+++ test-server/getUser.js
@@ -3,17 +3,18 @@ const { getUser } = require("../src/requests");
 
 describe("getUser", function () {
   it("should return all of a contributor's information in an object with values for: contributor_id, contributor_name, contributor_signature, and token", async function () {
-    const user = await getUser("0x0c55D3B26A1229B9D707a4272F55E66103301858");
-
-    assert.equal(
-      user.contributor_name,
-      "michael",
-      "Failed to get a contributor's information based on their id"
-    );
-    assert.equal(
-      user.id,
-      "0x0c55D3B26A1229B9D707a4272F55E66103301858",
-      "Failed to get a contributor's token based on their id"
-    );
-  });
+    assert.deepEqual(
+      await getUser(
+        /*contributor_id:*/ '0x0c55D3B26A1229B9D707a4272F55E66103301858'
+      ),
+      {
+        status: "success",
+        message: "User retrieved successfully",
+        info: {
+          contributor_id: "0x0c55D3B26A1229B9D707a4272F55E66103301858",
+          contributor_name: "michael"
+        }
+      }
+     )
+    });
 });
